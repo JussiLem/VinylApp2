341979622b5c4544e5d3ab7b0ca741dd9f49b96e
Jussi Lemmetyinen <jussi.lemmetyinen@siili.com>
2019-10-23 19:31:15 +0300

    First commit

---
+++ src/modules/views/ArtistReleaseForm.js
@@ -0,0 +1,111 @@
+import React from "react";
+import { Field, Form, FormSpy } from "react-final-form";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "./modules/components/Typography";
+import AppForm from "./modules/views/VinylAppForm";
+import { email, required } from "./modules/form/validation";
+import RFTextField from "./modules/form/RFTextField";
+import FormButton from "./modules/form/FormButton";
+import FormFeedback from "./modules/form/FormFeedback";
+import withRoot from "./modules/withRoot";
+
+const useStyles = makeStyles(theme => ({
+  form: {
+    marginTop: theme.spacing(6)
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(2)
+  },
+  feedback: {
+    marginTop: theme.spacing(2)
+  }
+}));
+
+const ArtistReleaseForm = () => {
+  const classes = useStyles();
+  const [sent, setSent] = React.useState(false);
+
+  const validate = values => {
+    const errors = required(["artist", "title"], values);
+
+    if (!errors.email) {
+      const emailError = email(values.email, values);
+      if (emailError) {
+        errors.email = email(values.email, values);
+      }
+    }
+
+    return errors;
+  };
+
+  const handleSubmit = () => {
+    setSent(true);
+  };
+
+  return (
+    <>
+      <AppForm>
+        <>
+          <Typography variant="h3" gutterBottom marked="center" align="center">
+            Add your album to the collection
+          </Typography>
+        </>
+        <Form
+          onSubmit={handleSubmit}
+          subscription={{ submitting: true }}
+          validate={validate}
+        >
+          {({ handleSubmit2, submitting }) => (
+            <form onSubmit={handleSubmit2} className={classes.form} noValidate>
+              <Field
+                autoComplete="email"
+                autoFocus
+                component={RFTextField}
+                disabled={submitting || sent}
+                fullWidth
+                label="Artist"
+                margin="normal"
+                name="artist"
+                required
+                size="large"
+              />
+              <Field
+                fullWidth
+                size="large"
+                component={RFTextField}
+                disabled={submitting || sent}
+                required
+                name="title"
+                autoComplete="artist-album-title"
+                label="Album title"
+                type="text"
+                margin="normal"
+              />
+              <FormSpy subscription={{ submitError: true }}>
+                {({ submitError }) =>
+                  submitError ? (
+                    <FormFeedback className={classes.feedback} error>
+                      {submitError}
+                    </FormFeedback>
+                  ) : null
+                }
+              </FormSpy>
+              <FormButton
+                className={classes.button}
+                disabled={submitting || sent}
+                size="large"
+                color="secondary"
+                fullWidth
+              >
+                {submitting || sent ? "In progress…" : "Save"}
+              </FormButton>
+            </form>
+          )}
+        </Form>
+      </AppForm>
+    </>
+  );
+};
+
+export default withRoot(ArtistReleaseForm);
